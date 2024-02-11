3b4ec9d1c3218fa9d989e3c856a3ab43027eb6fb
redwan-525 <redwantamim525@gmail.com>
2024-02-11 15:54:46 +0600

    add extra dark/light mode

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,55 +1,91 @@
 
 
-const Home = () => {
-  return (
-    <div>
-      <div className="navbar bg-fuchsia-200">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label
-              tabIndex={0}
-              className="btn btn-ghost lg:hidden text-sky-200"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              {/* Add dropdown items if needed */}
-            </ul>
-          </div>
-          <a className="btn btn-success normal-case text-xl text-bold text-white rounded-lg border-8">
-        Rahatutzaman Rizon
-          </a>
-        </div>
+import { useEffect, useState } from "react";
 
-        <div className="navbar-center lg:flex flex-col sm:flex-row">
-          <ul className="menu menu-horizontal px-1">
-            <li className="text-3xl font-bold bg-fuchsia-200">Todo App</li>
-          </ul>
-        </div>
 
-        <div className="navbar-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              {/* Add avatar content if needed */}
-            </div>
-          </label>
-        </div>
-      </div>
+
+import sun from "./light.png";
+import moon from "./dark.png";
+
+
+
+
+const Home= () => {
+    // use theme from local storage if available or set light theme
+    const [theme, setTheme] = useState(
+      localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+    );
+  
+    // update state on toggle
+    const handleToggle = (e) => {
+      if (e.target.checked) {
+        setTheme("dark");
+      } else {
+        setTheme("light");
+      }
+    };
+  
+    // set theme state in localstorage on mount & also update localstorage on state change
+    useEffect(() => {
+      localStorage.setItem("theme", theme);
+      const localTheme = localStorage.getItem("theme");
+      // add custom data-theme attribute to html tag required to update theme using DaisyUI
+      document.querySelector("html").setAttribute("data-theme", localTheme);
+    }, [theme]);
+
+    
+
+
+   
+    return (
+        <div>
+           <div className="navbar bg-sky-600">
+  <div className="navbar-start ">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden text-blue-400">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+     
+      </ul>
     </div>
-  );
+    <a className="btn btn-success normal-case text-xl text-bold text-white  rounded-lg border-8">React Todo App</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+
+    </ul>
+  </div>
+
+  <button className="btn btn-square btn-ghost">
+          <label className="swap swap-rotate w-12 h-12">
+            <input
+              type="checkbox"
+              onChange={handleToggle}
+              // show toggle image based on localstorage theme
+              checked={theme === "light" ? false : true}
+            />
+            {/* light theme sun image */}
+            <img src={sun} alt="light" className="w-8 h-8 swap-on" />
+            {/* dark theme moon image */}
+            <img src={moon} alt="dark" className="w-8 h-8 swap-off" />
+          </label>
+        </button>
+  
+  <div className="navbar-end">
+  <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+         
+        </div>
+      </label>
+      
+  </div>
+</div>
+
+
+</div>
+       
+    );
 };
 
 export default Home;
